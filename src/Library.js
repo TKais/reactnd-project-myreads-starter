5c8ab07deb322672bd0634809908d4ad3489fe85
TKais <TiffanyTKaiser@gmail.com>
2018-07-26 22:57:12 -0700

    refactor: Rename prop for shelf title
    Rename title prop for the shelf to shelfTitle so there's no confusion between shelf titles and book titles

--- src/Library.js
+++ src/Library.js
@@ -11,7 +11,7 @@ class Library extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            <Shelf title="Currently Reading" />
+            <Shelf shelfTitle="Currently Reading" />
             <div className="bookshelf">
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
