c5430ee4f1d4957428f164abc3b50a9d3be401b7
TKais <TiffanyTKaiser@gmail.com>
2018-07-30 18:38:09 -0700

    refactor: Make the Shelf component a stateless functional component

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,29 +2,32 @@ import React from 'react';
 import Book from './Book';
 import PropTypes from 'prop-types';
 
-class Shelf extends React.Component {
+function Shelf(props) {
+  const updatedBooks = props.books.map( (book) => {
+    let shelfName = props.shelfTitle.toLowerCase().split(' ').join('');
+    if(shelfName === book.shelf.toLowerCase()) {
+      return (
+        <Book key={book.id} bookTitle={book.title} id={book.id} author={book.authors} bookImage={ book.imageLinks.thumbnail } onShelfChange={props.onShelfChange} books={props.books} shelf={book.shelf} />
+      );
+    }
+  });
 
-	render() {
-    const updatedBooks = this.props.books.map( (book) => {
-      let shelfName = this.props.shelfTitle.toLowerCase().split(' ').join('');
-      if(shelfName === book.shelf.toLowerCase()) {
-        return (
-          <Book key={book.id} bookTitle={book.title} id={book.id} author={book.authors} bookImage={ book.imageLinks.thumbnail } onShelfChange={this.props.onShelfChange} books={this.props.books} shelf={book.shelf} />
-        );
-      }
-    });
-
-		return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            { updatedBooks }
-          </ol>
-        </div>
+	return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.shelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          { updatedBooks }
+        </ol>
       </div>
-		);
-	}
+    </div>
+	);
 }
+
+Shelf.propTypes = {
+  shelfTitle: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  onShelfChange: PropTypes.func.isRequired,
+};
 
 export default Shelf;
