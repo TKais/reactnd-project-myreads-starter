dd61142295fb8b3ad8a493f323e20ab7e8320698
TKais <TiffanyTKaiser@gmail.com>
2018-07-30 19:09:33 -0700

    refactor: Fix spacing in Title component

--- src/Title.js
+++ src/Title.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Title(props) {
-	return (
-		<div className="list-books-title">
+  return (
+    <div className="list-books-title">
       <h1>{ props.pageTitle }</h1>
     </div>
-	);
+  );
 }
 
 Title.propTypes = {
