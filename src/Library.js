68e69584092867e07b73ac35e97da03b1eb71436
TKais <TiffanyTKaiser@gmail.com>
2018-07-30 18:33:38 -0700

    refactor: Make the Library component a stateless functional component

--- src/Library.js
+++ src/Library.js
@@ -3,24 +3,22 @@ import { Link } from 'react-router-dom';
 import Shelf from './Shelf';
 import Title from './Title';
 
-class Library extends React.Component {
-	render() {
-		return (
-      <div className="list-books">
-        <Title pageTitle="MyReads" />
-        <div className="list-books-content">
-          <div>
-            <Shelf shelfTitle="Currently Reading" books={this.props.allBooks} onShelfChange={this.props.onShelfChange} />
-            <Shelf shelfTitle="Want to Read" books={this.props.allBooks} onShelfChange={this.props.onShelfChange} />
-            <Shelf shelfTitle="Read" books={this.props.allBooks} onShelfChange={this.props.onShelfChange}  />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+function Library(props) {
+	return (
+    <div className="list-books">
+      <Title pageTitle="MyReads" />
+      <div className="list-books-content">
+        <div>
+          <Shelf shelfTitle="Currently Reading" books={props.allBooks} onShelfChange={props.onShelfChange} />
+          <Shelf shelfTitle="Want to Read" books={props.allBooks} onShelfChange={props.onShelfChange} />
+          <Shelf shelfTitle="Read" books={props.allBooks} onShelfChange={props.onShelfChange}  />
         </div>
       </div>
-    );
-	}
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Library;
